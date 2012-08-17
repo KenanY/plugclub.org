aba73c34527400ae617b5bcdaafa9c12152d210f
Kenan Yildirim <kenan@kenany.me>
2012-08-16 22:36:02 -0400

    Add Foundation alerts

--- source/js/plugins.js
+++ source/js/plugins.js
@@ -1266,3 +1266,27 @@ jQuery(document).ready(function ($) {
     }
   };
 })(jQuery);
+
+
+
+
+
+(function ($) {
+
+  $.fn.foundationAlerts = function (options) {
+    var settings = $.extend({
+      callback: $.noop
+    }, options);
+
+    $(document).on("click", ".alert-box a.close", function (event) {
+      event.preventDefault();
+      $(this).closest(".alert-box").fadeOut(function (event) {
+        $(this).remove();
+        // Do something else after the alert closes
+        settings.callback();
+      });
+    });
+
+  };
+
+})(jQuery);
