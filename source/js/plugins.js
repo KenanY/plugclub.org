0589308b1a7b6d732ac23091eefd8a38133ff1dc
Kenan Yildirim <kenan@kenany.me>
2012-08-16 22:37:08 -0400

    Add Foundation accordian

--- source/js/plugins.js
+++ source/js/plugins.js
@@ -1344,3 +1344,24 @@ jQuery(document).ready(function ($) {
   };
 
 })( jQuery );
+
+
+
+
+
+(function ($){
+
+  $.fn.foundationAccordion = function (options) {
+
+    $('.accordion li', this).on('click.fndtn', function () {
+    var p = $(this).parent(); //changed this
+      var flyout = $(this).children('.content').first();
+      $('.content', p).not(flyout).hide().parent('li').removeClass('active'); //changed this
+      flyout.show(0, function () {
+        flyout.parent('li').addClass('active');
+      });
+    });
+
+  };
+
+})( jQuery );
